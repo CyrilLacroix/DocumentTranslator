4db6db94f6923dfe3e648570dedec406a2774b39
Cyril LACROIX <cyril.lacroix@bechtle.com>
2021-03-12 10:07:17 +0100

    Migrate to .NET5

---
+++ TranslationAssistantCore.Business/ReplaceLanguageCode.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+
+namespace TranslationAssistantCore.Business
+{
+    static class ReplaceLanguageCode
+    {
+        public static string Replace(string text, string fromlangcode, string tolangcode)
+        {
+            return Regex.Replace(text, fromlangcode + "-{,1}[a-z]{2,4}", tolangcode, RegexOptions.IgnoreCase);
+        }
+    }
+}
---
+++ TranslationAssistantCore.Business/TranslateList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TranslationAssistantCore.TranslationServices.Core;
+
+namespace TranslationAssistantCore.Business
+{
+    public class TranslateList
+    {
+        public TranslateList()
+        {
+
+        }
+
+        public async Task<List<string>> Translate(List<string> list, string from, string to)
+        {
+            List<string> translatedlist = new List<string>();
+            List<Task<string>> tasklist = new List<Task<string>>();
+            foreach (string text in list)
+            {
+                Task<string> task = TranslationServiceFacade.TranslateStringAsync(text, from, to);
+                tasklist.Add(task);
+            }
+            string[] resultlist = await Task.WhenAll(tasklist);
+            return resultlist.ToList<string>();
+        }
+    }
+}
